52f77935dde5e46d1843a139e7f1678da26edfc9
Xenocyst <MichaelCermak.v@gmail.com>
2016-08-28 22:29:05 -0400

    Testing

--- Procedures.py
+++ Procedures.py
@@ -2,11 +2,12 @@
 page = ('<div id="top_bin"><div id="top_content" class="width960">'
         '<div class="udacity float-left"><a href="http://udacity.com">')
 
-start_link = page.find('<a href=')      # finds the string '<a href=' in var page
-start_quote = page.find('"', start_link)    # finds the first " located after the end of var start_link
-end_quote = page.find('"', start_quote + 1)     # finds the 2nd " located after var start_quote +1 (h)
-url = page[start_quote + 1: end_quote]      # creates the var "url" and selects the string between
-print(url)      # prints var url
+start_link = page.find('<a href=')  # finds the string '<a href=' in var page
+start_quote = page.find('"', start_link)  # finds the first " located after the end of var start_link
+end_quote = page.find('"', start_quote + 1)  # finds the 2nd " located after var start_quote +1 (h)
+url = page[start_quote + 1: end_quote]  # creates the var "url" and selects the string between
+print(url)  # prints var url
+
 
 # Automates above to find all links in the page
 
@@ -19,23 +20,33 @@ def get_next_target(page):
 
 print(get_next_target(page))
 
-# Basic procedure w/ input
 
-def rest_of_string(s):      # Creates the procedure "rest_of_string" with the input s.
+# Basic procedure w/ input -----------------------------
+
+def rest_of_string(s):  # Creates the procedure "rest_of_string" with the input s.
     return s[:]  # Same as string-in-string [1:] = "est", etc...
+
 
 print(rest_of_string('test'))  # Sets (s) in line 23 to the string "test" and prints depending on "return s[#:#]"
 
-# a becomes s and b becomes t, they still return for a
 
-def sum(a, b):
+def sum(a, b):  # a becomes s and b becomes t, they still return for a
     a = a + b
-    return(a)
+    return a
 
 s = 'Hello '
 t = 'Mike!'
 print(sum(s, t))
 
+
+def abbaize(a, b):  # var's (input's) are contained within the def
+    return a + b + b + a  # composition of var's
+
+print(abbaize('a', 'b'))  # strings
+print(abbaize('cat', 'dog'))  # strings
+
+
+# --------------------------------------------------------
 # Procedure Composition
 
 def square(n):
@@ -43,4 +54,11 @@ def square(n):
 
 x = 5
 print(square(square(x)))
+
+
+def find_second(search, target):
+    return search.find(target, search.find(target) + 1)
+
+space = "somewhere something is looking to be found somewhere"
+print(find_second(space, 'somewhere'))
 
